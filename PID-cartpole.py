75c1be5e049e9c64a74c4704b964ec86cf0343b5
prathamchintamani <prathamchintamani@gmail.com>
2024-12-19 19:31:21 +0530

    tuned PID

--- PID-cartpole.py
+++ PID-cartpole.py
@@ -1,13 +1,14 @@
 import torch
 import numpy as np
 import gymnasium as gym
+import time
 
 env = gym.make("CartPole-v1", render_mode = "human")
 episode_over = False
 score = 0
 observation, something = env.reset()
 kp1 = 0.9
-kd1 = 1
+kd1 = 0.9
 kp2 = 0.2
 kd2 = 0.1
 while not episode_over:
@@ -16,4 +17,5 @@ while not episode_over:
     observation, reward, truncated, terminated, info = env.step(action)
     episode_over = True if terminated or truncated else False
 
+time.sleep(1)
 env.close()
