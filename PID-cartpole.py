3b8a295b37651fa01285927cdff84cfcc7abc1dc
prathamchintamani <prathamchintamani@gmail.com>
2024-12-19 19:22:06 +0530

    implemented PID on CartPole-v1

---
+++ PID-cartpole.py
@@ -0,0 +1,19 @@
+import torch
+import numpy as np
+import gymnasium as gym
+
+env = gym.make("CartPole-v1", render_mode = "human")
+episode_over = False
+score = 0
+observation, something = env.reset()
+kp1 = 0.9
+kd1 = 1
+kp2 = 0.2
+kd2 = 0.1
+while not episode_over:
+    f = kp1 * observation[2] + kd1 * observation[3] + kp2 * observation[0] + kd2 * observation[1]
+    action = 1 if f > 0 else 0
+    observation, reward, truncated, terminated, info = env.step(action)
+    episode_over = True if terminated or truncated else False
+
+env.close()
